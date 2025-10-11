c27836ae2bfcc496999f5e29d5429217efdc5f8c
mm0108 <162650607+mm0108@users.noreply.github.com>
2025-10-11 20:29:25 +0900

    Add files via upload

---
+++ # Loyihani saqlash papkasi.rb
@@ -0,0 +1,10 @@
+# Loyihani saqlash papkasi
+mkdir FastAPI_Userguide
+cd FastAPI_Userguide
+
+# Fayllarni yaratish
+echo > main.py
+echo fastapi > requirements.txt
+echo uvicorn[standard] >> requirements.txt
+echo pydantic >> requirements.txt
+echo "# FastAPI User Guide Example"
